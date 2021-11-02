1963b867db3e83a33692fffabc6df0400e9a84a4
seroroman <47804854+seroroman@users.noreply.github.com>
2021-11-02 17:28:15 +0900

    追加機能

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
+
   const [incompleteTdos, setIncompleteTodos] = useState([
     "あああああ",
     "いいいいいい"
@@ -9,11 +11,26 @@ export const App = () => {
 
   const [completeTodos] = useState(["ううううう"]);
 
+  const onChangeTodoText = (event) => {
+    setTodoText(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTdos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="imcomplete-area">
         <p className="title">未完了のTODO</p>
